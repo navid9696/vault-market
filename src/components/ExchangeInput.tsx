f1840d227829417a3460195771faba822180387c
navid9696 <navid96@interia.pl>
2024-06-21 18:24:19 +0200

    ExchangeInputProps interface moved to ExchangeInput component, initial value of inputValue state is now empty string

--- src/components/ExchangeInput.tsx
+++ src/components/ExchangeInput.tsx
@@ -1,7 +1,14 @@
-import { ExchangeInputProps } from '@/lib/types'
+import { ChangeEventHandler } from 'react'
 import { FormControl, InputAdornment, InputLabel, OutlinedInput } from '@mui/material'
 
-const ExchangeInput = ({ icon, inputProp, onChange }: ExchangeInputProps) => {
+interface ExchangeInputProps {
+	icon?: React.ReactNode
+	inputProps?: { [key: string]: string | number | boolean | null }
+	onChange?: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>
+	value?: number | string
+}
+
+const ExchangeInput = ({ icon, inputProps, onChange, value }: ExchangeInputProps) => {
 	return (
 		<FormControl sx={{ m: 1, width: '35%' }}>
 			<InputLabel htmlFor='outlined-adornment-amount'>Amount</InputLabel>
@@ -11,8 +18,9 @@ const ExchangeInput = ({ icon, inputProp, onChange }: ExchangeInputProps) => {
 				startAdornment={<InputAdornment position='start'>{icon}</InputAdornment>}
 				onChange={onChange}
 				label='Amount'
-				onKeyDown={e => (e.key === '-' || e.key === '+' || e.key === '.' ? e.preventDefault() : false)}
-				inputProps={{ min: '0', ...inputProp }}
+				value={value}
+				onKeyDown={e => (e.key === '-' || e.key === '+' || e.key === '.' || e.key === 'e' ? e.preventDefault() : false)}
+				inputProps={{ min: '0', ...inputProps }}
 			/>
 		</FormControl>
 	)
--- src/components/ExchangeModal.tsx
+++ src/components/ExchangeModal.tsx
@@ -1,40 +1,47 @@
 import { Button, Divider, Typography } from '@mui/material'
 import { FaEquals as Equals } from 'react-icons/fa'
-import { GiBottleCap } from 'react-icons/gi'
+import { GiBottleCap as Caps } from 'react-icons/gi'
 import ExchangeInput from './ExchangeInput'
 import Image from 'next/image'
 import { ChangeEvent, useState } from 'react'
 
 const ExchangeModal = () => {
-	const [inputValue, setInputValue] = useState<number | null>(null)
+	const [inputValue, setInputValue] = useState<number | string>('')
 
 	const convertCurrency = (e: ChangeEvent<HTMLInputElement>) => {
 		const value = parseFloat(e.target.value)
 		if (!isNaN(value)) {
 			setInputValue(parseFloat((value * 0.05).toFixed(2)))
 		} else {
-			setInputValue(0)
+			setInputValue('')
 		}
 	}
-
 
 	return (
 		<div className='text-center'>
 			<Typography component={'h3'} variant='h3' gutterBottom>
 				Caps&Cash Exchange
 			</Typography>
-			<Typography component={'h4'} variant='h5' gutterBottom>
+			<Typography component={'h4'} variant='h6' gutterBottom>
 				How many caps are you looking to acquire?
 			</Typography>
 			<div className='flex items-center justify-between'>
 				<Typography className='m-auto' paragraph variant='h4'>
 					Caps
 				</Typography>
-				<ExchangeInput onChange={convertCurrency} icon={<GiBottleCap />} inputProp={{ required: true }} />
+				<ExchangeInput onChange={convertCurrency} icon={<Caps />} inputProps={{ required: true }} />
 			</div>
 			<div>
 				<Typography className='flex items-center justify-center' paragraph variant='body1' gutterBottom>
-					<Image className='mr-1' src={'/imgs/nuka-cap-alt.png'} width={20} height={20} alt='nuka cola bottle cap' />1
+					<Image
+						className='mr-1'
+						src={'/icons/nuka-cap-alt.png'}
+						width={15}
+						height={15}
+						alt='nuka cola bottle cap'
+						loading='lazy'
+					/>
+					1
 				</Typography>
 				<Divider className='my-4'>
 					<Equals className='rotate-90 scale-150' />
@@ -47,12 +54,12 @@ const ExchangeModal = () => {
 				<Typography className='m-auto' paragraph variant='h4'>
 					$USD
 				</Typography>
-				<ExchangeInput icon={'$'} inputProp={{ readOnly: true, value: inputValue }} />
+				<ExchangeInput icon={'$'} value={inputValue} inputProps={{ readOnly: true }} />
 			</div>
-			{/* add integration with database */}
-			<Button className='mt-5 font-extrabold' variant='outlined' size='large' endIcon={<GiBottleCap />}>
+			<Button className='mt-5 font-extrabold' variant='outlined' size='large' endIcon={<Caps />}>
 				BUY
 			</Button>
+			{/* add integration with database  */}
 		</div>
 	)
 }
