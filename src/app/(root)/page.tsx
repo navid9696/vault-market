75bad52a2e46004bc0bc17be726c6400ac0bfec4
Dawid Brzęczek <navid96@interia.pl>
2024-07-27 11:43:17 +0200

    Update src/app/(root)/page.tsx
    Co-authored-by: Bartłomiej Krakowski <48633090+bart-krakowski@users.noreply.github.com>

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -12,11 +12,6 @@ const Home = () => {
 		typography: {
 			fontFamily: ['__Share_Tech_Mono_3ec33c', '__Share_Tech_Mono_Fallback_3ec33c'].join(','),
 		},
-		// components: {
-		// 	MuiInputBase: {
-		// 		styleOverrides: { root: { fontFamily: ['__VT323_4ca395', '__VT323_Fallback_4ca395'].join(',') } },
-		// 	},
-		// },
 	})
 
 	return (
