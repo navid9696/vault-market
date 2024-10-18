72c3897b8c9f1a3c3d443fbfaa273b51c29e8a29
Dawid Brzęczek <navid96@interia.pl>
2024-10-18 20:27:51 +0200

    improved styles of product card in admin dashboard

--- src/components/AdminProductCard.tsx
+++ src/components/AdminProductCard.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
-import CardMedia from '@mui/material/CardMedia'
 import Typography from '@mui/material/Typography'
 import { Button, CardActionArea, CardActions } from '@mui/material'
 import { ProductCardProps } from './ProductCard'
@@ -9,37 +8,37 @@ import Image from 'next/image'
 
 const AdminProductCard = ({ name, price, rating, onSale, available, imgURL, description }: ProductCardProps) => {
 	return (
-		<Card sx={{ maxWidth: 345 }}>
+		<Card className='flex flex-col justify-between' sx={{ height: 450, width: 300, borderRadius: 3, boxShadow: 3, overflow: 'hidden' }}>
 			<CardActionArea>
-				<div>
-					<Image className='object-contain' src={imgURL} fill alt=''></Image>
+				<div className='relative h-48'>
+					<Image className='object-contain' src={imgURL} fill alt={name} />
 				</div>
 				<CardContent>
-					<Typography gutterBottom variant='h5' component='div'>
+					<Typography gutterBottom variant='h5' className='text-center font-semibold'>
 						{name}
 					</Typography>
-					<Typography variant='body2' color='text.secondary'>
+					<Typography variant='body2' color='text.secondary' className='mb-2'>
 						{description}
 					</Typography>
-					<Typography variant='body2' color='text.secondary'>
-						{price}
+					<Typography variant='body2' color='text.primary' className='font-medium'>
+						Price: ${price}
 					</Typography>
-					<Typography variant='body2' color='text.secondary'>
-						{rating}
+					<Typography variant='body2' color='text.primary' className='font-medium'>
+						Rating: {rating}/5
 					</Typography>
-					<Typography variant='body2' color='text.secondary'>
-						{onSale}
+					<Typography variant='body2' color={onSale ? 'error.main' : 'text.secondary'} className='font-medium'>
+						{onSale ? 'On Sale!' : 'Regular Price'}
 					</Typography>
-					<Typography variant='body2' color='text.secondary'>
-						{available}
+					<Typography variant='body2' color={available ? 'success.main' : 'error.main'} className='font-medium'>
+						{available ? 'In Stock' : 'Out of Stock'}
 					</Typography>
 				</CardContent>
 			</CardActionArea>
-			<CardActions>
-				<Button size='small' color='primary'>
+			<CardActions sx={{ justifyContent: 'space-between', padding: 2 }}>
+				<Button size='small' color='primary' variant='contained'>
 					Edit
 				</Button>
-				<Button size='small' color='primary'>
+				<Button size='small' color='secondary' variant='outlined'>
 					Delete
 				</Button>
 			</CardActions>
