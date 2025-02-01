0fe3af5668f4e3464ecf54a6ccfc7db153136c00
Dawid Brzęczek <navid96@interia.pl>
2025-02-01 12:08:45 +0100

    Update TrpcProvider.tsx

--- src/providers/TrpcProvider.tsx
+++ src/providers/TrpcProvider.tsx
@@ -11,7 +11,7 @@ export const TrpcProvider = ({ children }: PropsWithChildren) => {
 		trpc.createClient({
 			links: [
 				httpBatchLink({
-					url: 'http://localhost:3000/api/trpc',
+					url: '/api/trpc',
 				}),
 			],
 		})
